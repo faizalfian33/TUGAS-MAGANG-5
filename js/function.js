ef38b5fb9560c7aa856e5a2579323e5fbadda65e
faizalfian33 <59559035+faizalfian33@users.noreply.github.com>
2020-01-14 09:49:38 +0700

    Add files via upload

---
+++ js/function.js
@@ -0,0 +1,55 @@
+function myFunction() {
+		  var dots = document.getElementById("dots");
+		  var moreText = document.getElementById("more");
+		  var btnText = document.getElementById("myBtn");
+		  var tabku = document.getElementById("tab")
+		  if (dots.style.display === "none") {
+		    dots.style.display = "inline";
+		    btnText.innerHTML = "Read more";
+		    moreText.style.display = "none";
+		    tabku.style = "height: 200px"; 
+
+		  } else {
+		    dots.style.display = "none";
+		    btnText.innerHTML = "Read less";
+		    moreText.style.display = "inline";
+		    tabku.style = "height: 250px"; 
+		  }
+		}
+		function myFunctiona() {
+		  var dotsa = document.getElementById("dotsa");
+		  var moreTexta = document.getElementById("morea");
+		  var btnTexta = document.getElementById("myBtna");
+		  var tabkua = document.getElementById("taba")
+		  if (dotsa.style.display === "none") {
+		    dotsa.style.display = "inline";
+		    btnTexta.innerHTML = "Read more";
+		    moreTexta.style.display = "none";
+		    tabkua.style = "height: 200px"; 
+
+		  } else {
+		    dotsa.style.display = "none";
+		    btnTexta.innerHTML = "Read less";
+		    moreTexta.style.display = "inline";
+		    tabkua.style = "height: 250px"; 
+		  }
+		}
+
+		function myFunctionb() {
+		  var dotsb = document.getElementById("dotsb");
+		  var moreTextb = document.getElementById("moreb");
+		  var btnTextb = document.getElementById("myBtnb");
+		  var tabkub = document.getElementById("tabb")
+		  if (dotsb.style.display === "none") {
+		    dotsb.style.display = "inline";
+		    btnTextb.innerHTML = "Read more";
+		    moreTextb.style.display = "none";
+		    tabkub.style = "height: 200px"; 
+
+		  } else {
+		    dotsb.style.display = "none";
+		    btnTextb.innerHTML = "Read less";
+		    moreTextb.style.display = "inline";
+		    tabkub.style = "height: 250px"; 
+		  }
+		}
